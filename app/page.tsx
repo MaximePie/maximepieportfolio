e0df170afb0a91d8d0dee81435bd8a803cfc56c7
MaximePie <maxime.pie.mail@gmail.com>
2025-11-14 17:23:51 +0100

    First Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,84 @@
 import Image from "next/image";
 
-export default function Home() {
+function PillarCard({
+  title,
+  description,
+  imageUrl,
+}: {
+  title: string;
+  description: string;
+  imageUrl: string;
+}) {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <div
+      className="bg-linear-to-b from-white to-indigo-50
+     h-80 relative border p-4 m-4 rounded-lg border-sky-200/50 shadow-lg w-2xs overflow-hidden"
+    >
+      <div
+        className="absolute bottom-0 right-0 w-32 h-32 opacity-50 bg-no-repeat bg-contain bg-bottom bg-right pointer-events-none"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className="relative z-10">
+        <h3 className="text-lg font-semibold">{title}</h3>
+        <p className="mt-2">{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function Home() {
+  const pillars = [
+    {
+      title: "Performance",
+      description:
+        "Je m'efforce de créer des applications web rapides et réactives pour offrir une expérience utilisateur optimale.",
+      imageUrl: "/performance.svg",
+    },
+    {
+      title: "Accessibilité",
+      description:
+        "Je veille à ce que mes applications soient accessibles à tous, en respectant les normes d'accessibilité web.",
+      imageUrl: "/accessibility.svg",
+    },
+    {
+      title: "Sécurité",
+      description:
+        "La sécurité est une priorité dans mes développements, en intégrant les meilleures pratiques pour protéger les données des utilisateurs.",
+      imageUrl: "/security.svg",
+    },
+  ];
+  return (
+    <div className="p-8 bg-gradient-to-b from-indigo-200 via-purple-200 to-pink-200">
+      <div className="flex items-center justify-between mb-12">
+        <div>
+          <h1 className="text-4xl font-bold">Maxime Pie</h1>
+          <h2 className="text-2xl">Développeur passioné</h2>
+          <p className="mt-4">
+            Bonjour je m&apos;appelle Maxime Pie et je suis ravi de vous
+            accueillir sur mon portfolio.
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+        <Image
+          src="/magrossetete.jpg"
+          alt="Photo de Maxime Pie"
+          width={192}
+          height={192}
+          className="rounded-full mt-4"
+        />
+      </div>
+
+      <h2 className="text-3xl">Pilliers de développement</h2>
+
+      <div className="flex flex-wrap">
+        {pillars.map((pillar) => (
+          <PillarCard
+            key={pillar.title}
+            title={pillar.title}
+            description={pillar.description}
+            imageUrl={pillar.imageUrl}
+          />
+        ))}
+      </div>
     </div>
   );
 }
